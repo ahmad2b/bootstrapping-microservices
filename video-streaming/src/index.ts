a22ed156ec3504a2c4ba4a9da541ac5d2dfe36d9
Muhammad Ahmad <ahmadshaukat_4@outlook.com>
2024-02-28 16:34:28 +0500

    Add MongoDB container and update video streaming service configuration

--- video-streaming/src/index.ts
+++ video-streaming/src/index.ts
@@ -1,7 +1,6 @@
 import express from 'express';
 import http from 'http';
-
-const app = express();
+import mongodb from 'mongodb';
 
 if (!process.env.PORT) {
 	throw new Error(
@@ -21,33 +20,66 @@ if (!process.env.VIDEO_STORAGE_PORT) {
 	);
 }
 
+if (!process.env.DBHOST) {
+	throw new Error(
+		'Please specify the host name for the database in variable DBHOST.'
+	);
+}
+
+if (!process.env.DBNAME) {
+	throw new Error('Please specify the database name in variable DBNAME.');
+}
+
 const PORT = process.env.PORT;
 const VIDEO_STORAGE_HOST = process.env.VIDEO_STORAGE_HOST;
 const VIDEO_STORAGE_PORT = parseInt(process.env.VIDEO_STORAGE_PORT);
+const DBHOST = process.env.DBHOST;
+const DBNAME = process.env.DBNAME;
 
 console.log(
 	`Forwarding video requests to ${VIDEO_STORAGE_HOST}:${VIDEO_STORAGE_PORT}.`
 );
 
-app.get('/video', async (req, res) => {
-	const forwardRequest = http.request(
-		{
-			hostname: VIDEO_STORAGE_HOST,
-			port: VIDEO_STORAGE_PORT,
-			path: '/video?path=video.mp4',
-			method: 'GET',
-			headers: req.headers,
-		},
-		(forwardResponse) => {
-			const statusCode = forwardResponse.statusCode ?? 502; // Using 502 as a generic "Bad Gateway" error
-			res.writeHead(statusCode, forwardResponse.headers);
-			forwardResponse.pipe(res);
+async function main() {
+	const client = await mongodb.MongoClient.connect(DBHOST);
+	const db = client.db(DBNAME);
+	const videoCollection = db.collection('videos');
+
+	const app = express();
+
+	app.get('/video', async (req, res) => {
+		const videoId = new mongodb.ObjectId(req.query.id as string);
+		const videoRecord = await videoCollection.findOne({ _id: videoId });
+
+		if (!videoRecord) {
+			res.sendStatus(404);
+			return;
 		}
-	);
 
-	req.pipe(forwardRequest);
-});
+		const forwardRequest = http.request(
+			{
+				hostname: VIDEO_STORAGE_HOST,
+				port: VIDEO_STORAGE_PORT,
+				path: `/video?path=${videoRecord.videoPath}`,
+				method: 'GET',
+				headers: req.headers,
+			},
+			(forwardResponse) => {
+				const statusCode = forwardResponse.statusCode ?? 502; // Using 502 as a generic "Bad Gateway" error
+				res.writeHead(statusCode, forwardResponse.headers);
+				forwardResponse.pipe(res);
+			}
+		);
 
-app.listen(PORT, () => {
-	console.log(`Video Streaming service is online`);
+		req.pipe(forwardRequest);
+	});
+
+	app.listen(PORT, () => {
+		console.log(`Video Streaming service is online`);
+	});
+}
+
+main().catch((err) => {
+	console.error('Microservices failed to start');
+	console.error((err && err.stack) || err);
 });
