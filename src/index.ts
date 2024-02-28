199ba2db23c28644da078629e85c39d1e5b5b342
Muhammad Ahmad <ahmadshaukat_4@outlook.com>
2024-02-28 08:27:43 +0500

    Initialized typescript

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import { Request, Response } from 'express';
+import fs from 'fs';
+
+import express from 'express';
+
+const app = express();
+
+if (!process.env.PORT) {
+	throw new Error(
+		'Please specify the port number for the HTTP server with the environment variable PORT.'
+	);
+}
+
+const port = process.env.PORT;
+
+app.get('/video', async (req: Request, res: Response) => {
+	const videoPath = './video.mp4';
+	const stats = await fs.promises.stat(videoPath);
+
+	res.writeHead(200, {
+		'Content-Length': stats.size,
+		'Content-Type': 'video/mp4',
+	});
+	fs.createReadStream(videoPath).pipe(res);
+});
+
+app.listen(port, () => {
+	console.log(`Server is running on port ${port}`);
+});
