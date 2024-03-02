d3bb3d9ceed1636016e3a7ca9121968e204108b0
Muhammad Ahmad <ahmadshaukat_4@outlook.com>
2024-03-02 21:46:39 +0500

    Add Docker configuration and files for history service

---
+++ history/src/index.ts
@@ -0,0 +1,28 @@
+import express from 'express';
+
+if (!process.env.PORT) {
+	throw new Error(
+		'Please specify the port number for the HTTP server with the environment variable PORT.'
+	);
+}
+
+const PORT = process.env.PORT;
+
+async function main() {
+	console.log('Starting history service...');
+	const app = express();
+
+	app.listen(PORT, () => {
+		console.log(`History service is online!`, { port: PORT });
+	});
+
+	app.get('/history', (req, res) => {
+		res.json({ message: 'Hello history service!' });
+	});
+}
+
+main().catch((err) => {
+	console.error('History service failed to start');
+	console.error((err && err.stack) || err);
+	process.exit(1);
+});
