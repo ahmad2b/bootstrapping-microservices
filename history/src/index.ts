b90c5e002635c02f3c2f1455df508a90af31b8bb
Muhammad Ahmad <ahmadshaukat_4@outlook.com>
2024-03-03 12:53:44 +0500

    Update Dockerfiles and package.json dependencies

--- history/src/index.ts
+++ history/src/index.ts
@@ -1,3 +1,4 @@
+import amqp from 'amqplib';
 import express from 'express';
 import mongodb from 'mongodb';
 
@@ -19,6 +20,14 @@ if (!process.env.DBNAME) {
 	);
 }
 
+if (!process.env.RABBIT) {
+	throw new Error(
+		'Please specify the name of the RabbitMQ host using environment variable RABBIT'
+	);
+}
+
+const RABBIT = process.env.RABBIT;
+
 const PORT = process.env.PORT;
 const DBHOST = process.env.DBHOST;
 const DBNAME = process.env.DBNAME;
@@ -30,6 +39,45 @@ async function main() {
 	const client = await mongodb.MongoClient.connect(DBHOST);
 	const db = client.db(DBNAME);
 	const historyCollection = db.collection('history');
+
+	const messagingConnection = await amqp.connect(RABBIT);
+
+	console.log('Connected to RabbitMQ');
+
+	const messageChannel = await messagingConnection.createChannel();
+
+	await messageChannel.assertQueue('viewed', {});
+
+	console.log('Created "viewed" queue');
+
+	await messageChannel.consume('viewed', async (msg) => {
+		console.log('Received "viewed" message');
+
+		const parsedMsg = JSON.parse(msg?.content.toString() || '');
+
+		await historyCollection.insertOne({ videoPath: parsedMsg.videoPath });
+
+		console.log('Acknowledge message was handled');
+
+		messageChannel.ack(msg as amqp.Message);
+	});
+
+	app.get('/history', async (req, res) => {
+		const skip = parseInt(req.query.skip) || 0;
+		const limit = parseInt(req.query.limit) || 20;
+
+		const history = await historyCollection
+			.find()
+			.skip(skip)
+			.limit(limit)
+			.toArray();
+
+		res.json({ history });
+	});
+
+	app.listen(PORT, () => {
+		console.log(`Microservice online`);
+	});
 
 	app.post('/viewed', async (req, res) => {
 		const videoPath = req.body.videoPath;
--- video-streaming/src/index.ts
+++ video-streaming/src/index.ts
@@ -1,6 +1,6 @@
+import amqp from 'amqplib';
 import express from 'express';
 import fs from 'fs';
-import http from 'http';
 
 if (!process.env.PORT) {
 	throw new Error(
@@ -30,42 +30,61 @@ if (!process.env.DBNAME) {
 	throw new Error('Please specify the database name in variable DBNAME.');
 }
 
+if (!process.env.RABBIT) {
+	throw new Error(
+		'Please specify the name of the RabbitMQ host using environment variable RABBIT'
+	);
+}
+
 const PORT = process.env.PORT;
+const RABBIT = process.env.RABBIT;
 const VIDEO_STORAGE_HOST = process.env.VIDEO_STORAGE_HOST;
 const VIDEO_STORAGE_PORT = parseInt(process.env.VIDEO_STORAGE_PORT);
 const DBHOST = process.env.DBHOST;
 const DBNAME = process.env.DBNAME;
 
-console.log(
-	`Forwarding video requests to ${VIDEO_STORAGE_HOST}:${VIDEO_STORAGE_PORT}.`
-);
+// console.log(
+// 	`Forwarding video requests to ${VIDEO_STORAGE_HOST}:${VIDEO_STORAGE_PORT}.`
+// );
 
-const sendViewedMessage = (videoPath: string) => {
-	const postOptions = {
-		method: 'POST',
-		headers: { 'Content-Type': 'application/json' },
-	};
+const sendViewedMessage = (videoPath: string, messageChannel: amqp.Channel) => {
+	console.log(`Publishing message on "viewed" queue.`);
+	const msg = { videoPath: videoPath };
+	const jsonMsg = JSON.stringify(msg);
+	messageChannel.publish('', 'viewed', Buffer.from(jsonMsg)); // Publishes message to the "viewed" queue.
 
-	const requestBody = {
-		videoPath,
-	};
+	// const postOptions = {
+	// 	method: 'POST',
+	// 	headers: { 'Content-Type': 'application/json' },
+	// };
 
-	const req = http.request(`http://history/viewed`, postOptions);
+	// const requestBody = {
+	// 	videoPath,
+	// };
 
-	req.on('close', () => {
-		console.log('Sent "viewed" message to history microservice');
-	});
+	// const req = http.request(`http://history/viewed`, postOptions);
 
-	req.on('error', (err) => {
-		console.error('Failed to send "viewed" message to history microservice');
-		console.error((err && err.stack) || err);
-	});
+	// req.on('close', () => {
+	// 	console.log('Sent "viewed" message to history microservice');
+	// });
 
-	req.write(JSON.stringify(requestBody));
-	req.end();
+	// req.on('error', (err) => {
+	// 	console.error('Failed to send "viewed" message to history microservice');
+	// 	console.error((err && err.stack) || err);
+	// });
+
+	// req.write(JSON.stringify(requestBody));
+	// req.end();
 };
 
 async function main() {
+	console.log(`Connecting to RabbitMQ server at ${RABBIT}.`);
+
+	const messagingConnection = await amqp.connect(RABBIT); // Connects to the RabbitMQ server.
+	console.log('Connected to RabbitMQ.');
+
+	const messageChannel = await messagingConnection.createChannel(); // Creates a RabbitMQ messaging channel.
+
 	// const client = await MongoClient.connect(DBHOST);
 	// const db = client.db(DBNAME);
 	// const videoCollection = db.collection('videos');
@@ -110,7 +129,7 @@ async function main() {
 
 		fs.createReadStream(videoPath).pipe(res);
 
-		sendViewedMessage(videoPath);
+		sendViewedMessage(videoPath, messageChannel);
 	});
 
 	app.listen(PORT, () => {
